58be390b8c3ed4b5c35449b818aca3cf8590f02a
Kimano Lambert <67758469+manomadethis@users.noreply.github.com>
2024-01-31 23:05:26 -0500

    Added a logo

--- project/app/static/common/common.js
+++ project/app/static/common/common.js
@@ -1,10 +1,16 @@
 window.addEventListener('load', function() {
-  if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-    document.getElementById('search-icon').src = '../common/img/search-icon-dark.svg';
-    }
-  });
+  let preferredColorScheme = localStorage.getItem('preferred-color-scheme');
+  if (!preferredColorScheme) {
+    preferredColorScheme = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+    localStorage.setItem('preferred-color-scheme', preferredColorScheme);
+  }
+  const logoSrc = preferredColorScheme === 'dark' ? '../common/img/degree-planner-logo.png' : '../common/img/degree-planner-logo-dark.png';
+  document.getElementById('degree-planner-logo').src = logoSrc;
+});
 
 window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
-  const newImageSrc = e.matches ? '../common/img/search-icon-dark.svg' : '../common/img/search-icon.svg';
-  document.getElementById('search-icon').src = newImageSrc;
+  const newColorScheme = e.matches ? 'dark' : 'light';
+  localStorage.setItem('preferred-color-scheme', newColorScheme);
+  const newImageSrc = newColorScheme === 'dark' ? '../common/img/degree-planner-logo.png' : '../common/img/degree-planner-logo-dark.png';
+  document.getElementById('degree-planner-logo').src = newImageSrc;
 });
